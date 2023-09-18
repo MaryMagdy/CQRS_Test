3f210f6e6ffa28bf42dc03b7a29bc7b8daa21707
MaryMagdy <marry_magdy@yahoo.com>
2023-09-18 16:13:36 +0300

    first publish

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using CQRS_Lib.Data.Models;
+using CQRS_Lib.Reos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        public ItemsController(IItemsRepo repo)
+        {
+            _repo = repo;
+        }
+        private readonly IItemsRepo _repo;
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetItems()
+        {
+            return Ok(_repo.GetItems());
+        }
+        [HttpPost]
+        public async Task<IActionResult> InsertItem(Items item) 
+        {
+            _repo.InsertItems(item);
+            return Ok();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using CQRS_Lib.Data;
+using CQRS_Lib.Reos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(x=>x.UseSqlServer(
+  builder.Configuration.GetConnectionString("myCon")));
+
+builder.Services.AddScoped<IItemsRepo, ItemRepo>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
